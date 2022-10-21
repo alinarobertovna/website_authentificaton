b60c568e228b3513f4b0aabaab5b5f01cef327b2
Alina <alinarobertovna17.03@gmail.com>
2022-10-21 03:37:20 -0400

    connect to mongodb Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports  = 
 {
-    "URI": "mongodb://localhost/book_store"
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://alina:24CaFTmyCn9RBOhd@mongodbserver.lkhsa9n.mongodb.net/book_store?retryWrites=true&w=majority"
 }
